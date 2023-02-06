67fc864618eba21e91948e53ac3895a783639ddb
markoi95 <marko.ivanovic95@outlook.com>
2023-02-06 23:53:50 +0100

    klasa rezervacija sa CRUD v1

---
+++ model/rezervacija.php
@@ -0,0 +1,47 @@
+<?php
+
+class Rez {
+
+    public $rezID;
+    public $sto;
+    public $datumRez;
+    public $opis;
+    public $korisnik;
+    public $timestamp;
+
+    public function __construct($rezID = null, $sto = null, $datumRez = null, $opis = null, $korisnik = null, $timestamp = null)
+    {
+        $this->rezID = $rezID;
+        $this->sto = $sto;
+        $this->datumRez = $datumRez;
+        $this->opis = $opis;
+        $this->korisnik = $korisnik;
+        $this->timestamp = $timestamp;
+    }
+
+    public static function getAll(mysqli $conn) //dobija konekciju sa bazom kao ulazni element
+    {
+        $q = "SELECT * FROM rez";               //u varijablu q upisi sve kolone iz tabele rez
+        return $conn->query($q);                //objektno orijentisan nacin za vracanje query-a kao rezultata
+    }
+    public static function deleteById($rezID, mysqli $conn) //ulazni element id koji cemo da obrisemo i konekcija sa bazom
+    {
+        $q = "DELETE FROM rez WHERE rezID=$rezID"; //kveri za brisanje id-a iz tabele
+        return $conn->query($q);                   //vracanje tabele (bez obrisanog id-a)
+    }
+    public static function add($sto, $datumRez, $korisnik, mysqli $conn) // svi atributi objekta osim id-a, sam se generise
+    {
+        $q = "INSERT INTO rez(sto, datumRez, korisnik) values('$sto', '$datumRez', '$korisnik')"; //kveri za ubacivanje u tabelu
+        return $conn->query($q); //vraca tabelu sa ubacenim id-em (novom rezervacijom) 
+    }
+
+    public function update(mysqli $conn)
+    {
+        $query = "UPDATE rez SET sto=$this->sto, datumRez=$this->datumRez, korisnik=$this->korisnik  WHERE id=$this->stoID";
+        return $conn->query($query); //vraca tabelu sa update-ovanim sadrzajem 
+    }
+
+}
+
+
+?>
